42057ad1f378da432e5447649a11afcb0acc8ee6
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-09-30 20:17:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,6 @@ end
 
 def batch_badge_creator(attendees)
   badges = []
-  attendees.each do {|name| badges << badge_maker(attendee)}
-  end
-  badges
+  attendees.each do |name| 
+    badges << badge_maker(attendee)
 end
