07e74a64c14d91bde36b18b3651f32680cc39c42
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-09-30 20:55:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,9 @@ def assign_rooms(attendees)
     room_assignments
 end
 
-def printer(attendees,name)
-  puts badge_maker(name)
-  puts assign_rooms(attendees)
+def printer(attendees)
+  batch_badge_creator(attendees).each_with_index {|badge, index|
+  puts  badge
+  puts  room_assignments(index)
+  }
 end
