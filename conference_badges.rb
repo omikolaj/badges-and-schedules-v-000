5bb14a272daa9909b04177b38fa669fed36bbbf9
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-09-30 21:10:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,9 +18,6 @@ end
 require 'pry'
 def printer(attendees)
   batch_badge_creator(attendees).each do { |conference_badges|
-    puts conference_badges
-  }
-puts assign_rooms(attendees).each do { |room|
-    room
-  }
+    puts conference_badges}
+puts assign_rooms(attendees).each do { |room| puts room}
 end
