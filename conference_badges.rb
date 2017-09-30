197c646ac25efdf89932b686240e2423a6b7e26b
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-09-30 20:15:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,10 @@
 # Write your code here.
-def badge_maker(badges)
+def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-
+  badges = []
+  attendees.each do {|attendee| badges = badge_maker(attendee)}
+  badges
 end
